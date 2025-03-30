f68bdc046cea0374a401a466103822974d497d2c
Emircaan <screaps52@hotmai.com>
2025-03-30 17:50:14 +0300

    feat:LoadBalancer added

--- cmd/backend/server.go
+++ cmd/backend/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net"
+	"os"
 	"strings"
 
 	"github.com/Emircaan/reverse-proxy/pkg/proto"
@@ -12,27 +13,31 @@ import (
 
 type BackendServer struct {
 	proto.UnimplementedProxyServiceServer
+	port string
 }
 
 func (s *BackendServer) ForwardRequest(ctx context.Context, req *proto.Request) (*proto.Response, error) {
-	log.Println("Backend: İstek Alindi:", req.Message)
+	log.Printf("Backend %s İstek Alindi:", s.port)
 	processedMessage := strings.ToUpper(req.Message)
 	log.Println("Backend: Gelen Mesaj İslendi: ", processedMessage)
 	return &proto.Response{Result: processedMessage}, nil
 }
 
 func main() {
-
-	listener, err := net.Listen("tcp", ":50052")
+	port := os.Getenv("BACKEND_PORT")
+	if port == "" {
+		port = "50052"
+	}
+	listener, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		log.Fatalf("Backend sunucusu başlatılamadı: %v", err)
+		log.Fatalf("Backend %s sunucusu başlatılamadı: %v", port, err)
 	}
 
 	grpcServer := grpc.NewServer()
-	proto.RegisterProxyServiceServer(grpcServer, &BackendServer{})
+	proto.RegisterProxyServiceServer(grpcServer, &BackendServer{port: port})
 
 	log.Println("Backend server çalışıyor: localhost:50052")
 	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatalf("Backend sunucusu çalıştırılamadı: %v", err)
+		log.Fatalf("Backend %s sunucusu çalıştırılamadı: %v", port, err)
 	}
 }
--- cmd/proxy/server.go
+++ cmd/proxy/server.go
@@ -13,39 +13,50 @@ import (
 
 type Server struct {
 	proto.UnimplementedProxyServiceServer
-	backendClient proto.ProxyServiceClient
+	backendClients []proto.ProxyServiceClient
+	currentIndex   int
 }
 
 func (s *Server) ForwardRequest(ctx context.Context, request *proto.Request) (*proto.Response, error) {
 	log.Println("Proxy: İstek Alındı:" + request.Message)
-
-	resp, err := s.backendClient.ForwardRequest(ctx, request)
+	backendIndex := s.currentIndex
+	backendClient := s.backendClients[backendIndex]
+	s.currentIndex = (s.currentIndex + 1) % len(s.backendClients)
+	resp, err := backendClient.ForwardRequest(ctx, request)
 	if err != nil {
-		return nil, fmt.Errorf("backend hatası: %v", err)
+		return nil, fmt.Errorf("backend'den cevap gelmedi %v", err)
 	}
-
 	log.Println("Proxy: Backend'den cevap alındı:", resp.Result)
 	return resp, nil
 
 }
 
 func main() {
-	conn, err := grpc.NewClient("localhost:50052", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("Backend'e bağlanırken hata olustu %v", err)
+	backendAdress := []string{
+		"localhost:50052",
+		"localhost:50053",
+		"localhost:50054",
 	}
-	defer conn.Close()
-	backendClient := proto.NewProxyServiceClient(conn)
-	listener, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatalf("Proxy sunucu başlatılamadı: %v", err)
+	var backendClients []proto.ProxyServiceClient
+	for _, address := range backendAdress {
+		conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("Backend sunucusuna bağlanılamadı: %v", err)
+		}
+		backendClients = append(backendClients, proto.NewProxyServiceClient(conn))
+	}
+	proxyServer := &Server{
+		backendClients: backendClients,
 	}
 	grpcServer := grpc.NewServer()
-	proto.RegisterProxyServiceServer(grpcServer, &Server{
-		backendClient: backendClient,
-	})
-	log.Println("Proxy server çalışıyor: localhost:50051")
-	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatalf("Proxy sunucusu çalıştırılamadı: %v", err)
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Proxy sunucusu başlatılamadı: %v", err)
+	}
+	proto.RegisterProxyServiceServer(grpcServer, proxyServer)
+	log.Println("Proxy server başlatıldı...")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("gRPC server başlatılamadı: %v", err)
 	}
 }
